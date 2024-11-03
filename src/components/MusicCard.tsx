538701892ec15ac50cef44187eeb34b14ec9907c
Angle-Brackets <ankitjoju@gmail.com>
2024-11-02 19:34:47 -0700

    Made buttons larger and added some more to controls

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,10 +1,7 @@
 import { IoMusicalNote } from "react-icons/io5";
-import { FaRegHeart, FaHeart } from "react-icons/fa";
-
 const MusicCard = () => {
     return (
         <div className="relative card flex clip items-center justify-center shadow-none bg-white w-full aspect-square rounded-xl overflow-hidden">
-            <FaHeart className="absolute top-2 right-2 text-xl text-gray-500 cursor-pointer" />
             <IoMusicalNote className="text-8xl text-gray-500" />
         </div>
     );
