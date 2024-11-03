4e5329ae816f7d31d6a2e0855a9832fd181529fa
Angle-Brackets <48231264+Angle-Brackets@users.noreply.github.com>
2024-11-02 19:34:47 -0700

    Made buttons larger and added some more to controls

--- src/components/MusicControls.jsx
+++ src/components/MusicControls.jsx
@@ -1,17 +1,18 @@
 import { FaPlayCircle, FaPauseCircle, FaFastForward, FaFastBackward, FaRegHeart, FaHeart, FaPlus} from "react-icons/fa";
-import { FaShuffle } from "react-icons/fa6";
+import { FaShuffle, FaRepeat } from "react-icons/fa6";
 
 const MusicControls = () => {
     return (
         <div className="flex flex-col items-center">
-            <progress className="progress w-80" value="23" max="100"/>
+            <progress className="progress w-[36rem]"/>
+
             <div className="flex flex-row text-3xl text-center">
-                <button className="btn btn-circle btn-ghost text-3xl">
+                <button className="btn btn-lg btn-circle btn-ghost text-5xl">
                     <FaFastBackward/>
                 </button>
 
                 <div className="tooltip" data-tip="Toggle Shuffle">
-                    <button className="btn btn-circle btn-ghost text-3xl">
+                    <button className="btn btn-lg btn-circle btn-ghost text-5xl">
                         <label className="swap">
                             <input type="checkbox"/>
                             <FaShuffle className="swap-on fill-cyan-200"/>
@@ -20,7 +21,15 @@ const MusicControls = () => {
                     </button>
                 </div>
 
-                <button className="btn btn-circle btn-ghost text-3xl">
+                <button className="btn btn-lg btn-circle btn-ghost text-5xl">
+                    <label className="swap">
+                        <input type="checkbox"/>
+                        <FaRegHeart className="swap-off fill-current"/>
+                        <FaHeart className="swap-on fill-current"/>
+                    </label>
+                </button>
+
+                <button className="btn btn-lg btn-circle btn-ghost text-5xl">
                     <label className="swap swap-rotate">
                         <input type="checkbox"/>
                         <FaPlayCircle className="swap-on fill-current"/>
@@ -28,10 +37,18 @@ const MusicControls = () => {
                     </label>
                 </button>
 
-                <button className="btn btn-circle btn-ghost text-3xl">
+                <button className="btn btn-lg btn-circle btn-ghost text-5xl">
+                    <label className="swap">
+                        <input type="checkbox"/>
+                        <FaRepeat className="swap-on fill-current fill-cyan-200"/>
+                        <FaRepeat className="swap-off fill-current"/>
+                    </label>
+                </button>
+
+                <button className="btn btn-lg btn-circle btn-ghost text-5xl">
                     <FaPlus/>
                 </button>
-                <button className="btn btn-circle btn-ghost text-3xl">
+                <button className="btn btn-lg btn-circle btn-ghost text-5xl">
                     <FaFastForward/>
                 </button>
             </div>
--- src/components/MusicInfo.jsx
+++ src/components/MusicInfo.jsx
@@ -2,10 +2,10 @@
 const MusicInfo = () => {
     return (
         <div className="text-center pb-4">
-            <div className="text-4xl">
+            <div className="text-6xl font-bold">
                 Song Name Here
             </div>
-            <div className="text-3xl mt-3">
+            <div className="text-3xl mt-3 ">
                 Artist Here
             </div>
         </div>
